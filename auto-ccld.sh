410af1dbe80ace68afc391d91c55367c871741e1
Angella Benneth <angy@ibexdelta.com>
2011-03-18 14:47:42 -0400

    first commit

---
+++ auto-ccld.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# WARNING: This file was auto-generated. Do not edit!
+CC='cc -O2 -DTLS=20070408 -I/usr/local/ssl/include'
+LD='cc -s'
---
+++ received.h
@@ -0,0 +1,7 @@
+#ifndef RECEIVED_H
+#define RECEIVED_H
+
+extern void received();
+extern void originip();
+
+#endif
